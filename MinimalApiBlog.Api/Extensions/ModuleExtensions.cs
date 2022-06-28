67e81ee2d89f29b14dc93aab7d5bc36b676f0412
Ali Amiresmaeili <aliamiresmaeili@gmail.com>
2022-06-28 23:42:01 +0430

    #AddApplicationServices

---
+++ MinimalApiBlog.Api/Extensions/ModuleExtensions.cs
@@ -0,0 +1,23 @@
+﻿using MinimalApiBlog.Api.Contracts;
+
+namespace MinimalApiBlog.Api.Extensions
+{
+    public static class ModuleExtensions
+    {
+        public static WebApplication RegisterEndpoints(this WebApplication app)
+        {
+            if (app == null)
+            {
+                throw new ArgumentNullException(nameof(app));
+            }
+
+            var modules = app.Services.GetServices<IModule>();
+            foreach (var module in modules)
+            {
+                module.RegisterEndpoints(app);
+            }
+
+            return app;
+        }
+    }
+}
--- MinimalApiBlog.Api/Extensions/ServiceCollectionExtensions.cs
+++ MinimalApiBlog.Api/Extensions/ServiceCollectionExtensions.cs
@@ -1,18 +1,39 @@
-﻿namespace MinimalApiBlog.Api.Extensions
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using MinimalApiBlog.Api.Contracts;
+using MinimalApiBlog.Dal;
+
+namespace MinimalApiBlog.Api.Extensions
 {
     public static class ServiceCollectionExtensions
     {
 
         public static IServiceCollection AddApplicationServices(this IServiceCollection services,
-            WebApplicationBuilder builder )
+            WebApplicationBuilder builder)
         {
-
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-
-
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = builder.Environment.ApplicationName, Version = "v1" });
+                options.TagActionsBy(ta => new List<string> { ta.ActionDescriptor.DisplayName! });
+            });
+            var connectionString = builder.Configuration.GetConnectionString("SqlServer");
+            builder.Services.AddDbContext<ApplicationDbContext>(opt => opt.UseSqlServer(connectionString));
+            builder.Services.AddMediatR(typeof(Program));
+            builder.Services.AddAutoMapper(typeof(Program));
+            builder.Services.AddAllModules(typeof(Program));
             return services;
+        }
+
+        private static void AddAllModules(this IServiceCollection services, params Type[] types)
+        {
+            // Using the `Scrutor` to add all of the application's modules at once.
+            services.Scan(scan =>
+                scan.FromAssembliesOf(types)
+                    .AddClasses(classes => classes.AssignableTo<IModule>())
+                    .AsImplementedInterfaces()
+                    .WithSingletonLifetime());
         }
 
     }
